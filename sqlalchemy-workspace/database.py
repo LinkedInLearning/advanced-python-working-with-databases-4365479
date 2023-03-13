760bfe345608ef3073d36079ebd576b5ae30ed31
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 13:42:16 -0700

    Starter code for 02_09

--- sqlalchemy-workspace/database.py
+++ sqlalchemy-workspace/database.py
@@ -1,9 +1,3 @@
 import sqlalchemy
 
 engine = sqlalchemy.create_engine('sqlite:///movies.db', echo=True)
-
-with engine.connect() as conn:
-	result = conn.execute(sqlalchemy.text("SELECT * FROM Movies"))
-	for row in result:
-		print(row)
-	
