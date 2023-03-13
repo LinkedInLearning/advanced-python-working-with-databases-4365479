4924718dcbc23e957e1b397e3c6ff14de7dc1277
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 16:26:58 -0700

    Update database.py

--- mysql-sqlalchemy-workspace/database.py
+++ mysql-sqlalchemy-workspace/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import registry, create_engine
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import registry
 
 engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/projects',
 	echo=True)
