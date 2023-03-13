ea3d267c4dbe642f67827f150d8289aeea9e5144
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 11:21:55 -0700

    Starter file for 02_04

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import sqlite3
 connection = sqlite3.connect('movies.db')
 cursor = connection.cursor()
 
-cursor.execute('''INSERT INTO Movies VALUES('Taxi Driver', 'Martin Scorsese', 1976)''')
-cursor.execute('''SELECT * FROM Movies''')
 
-print(cursor.fetchone())
 
 connection.commit()
 connection.close()
