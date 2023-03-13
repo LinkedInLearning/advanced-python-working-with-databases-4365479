1cb17565574c85d49521788c68c73eb9ab08b095
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 11:12:54 -0700

    Starter code for 02_03

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect('movies.db')
-
 cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS Movies 
-	(Title TEXT, Director TEXT, Year INT)''')
+
 
 connection.commit()
 connection.close()
