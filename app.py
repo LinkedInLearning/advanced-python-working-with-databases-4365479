2a5221882d1d85977da9b23b0e96a946b5f8b39d
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 11:59:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import sqlite3
 connection = sqlite3.connect('movies.db')
 cursor = connection.cursor()
 
+release_year = (1985,)
 
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
