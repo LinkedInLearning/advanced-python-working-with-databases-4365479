2db73e3ab548388e4e66b87a216529869eb6dee7
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 13:56:32 -0700

    Implement 02_09

--- sqlalchemy-workspace/database.py
+++ sqlalchemy-workspace/database.py
@@ -1,3 +1,16 @@
 import sqlalchemy
 
 engine = sqlalchemy.create_engine('sqlite:///movies.db', echo=True)
+
+metadata = sqlalchemy.MetaData()
+
+movies_table = sqlalchemy.Table("Movies", metadata,
+	sqlalchemy.Column("title", sqlalchemy.Text),
+	sqlalchemy.Column("director", sqlalchemy.Text),
+	sqlalchemy.Column("year", sqlalchemy.Integer))
+
+metadata.create_all(engine)
+
+with engine.connect() as conn:
+	for row in conn.execute(sqlalchemy.select(movies_table)):
+		print(row)
