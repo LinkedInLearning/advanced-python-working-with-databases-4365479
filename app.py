6c02f2be807c252b4f86e63786b1693ec5fad200
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 11:49:42 -0700

    Implement 02_04

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import sqlite3
 connection = sqlite3.connect('movies.db')
 cursor = connection.cursor()
 
+famousFilms = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+('Back to the Future', 'Robert Zemeckis', 1985),
+('Moonrise Kingdom', 'Wes Anderson', 2012)]
 
+cursor.executemany('INSERT INTO Movies VALUES (?,?,?)', famousFilms)
+
+cursor.execute("SELECT * FROM Movies")
+
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
