3ff1b403f437c25816d0fc2aa430eb6baec85536
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 16:34:17 -0700

    Update database.py

--- mysql-sqlalchemy-workspace/database.py
+++ mysql-sqlalchemy-workspace/database.py
@@ -3,3 +3,18 @@ from sqlalchemy.orm import registry
 
 engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/projects',
 	echo=True)
+
+mapper_registry = registry()
+#mapper_registry.metadata
+
+Base = mapper_registry.generate_base()
+
+class Project(Base):
+	__tablename__ = 'projects'
+	project_id = Column(Integer, primary_key=True)
+	title = Column(String(length=50))
+	description = Column(String(length=50))
+
+	def __repr__(self):
+		return "<Project(title='{0}, description='{1}')>".format(
+			self.title, self.description)
