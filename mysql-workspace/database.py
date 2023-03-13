8c9c6194d12c5787229c47cffa1683e46d3b20c9
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-13 15:56:07 -0700

    Starter code for 03_06

--- mysql-workspace/database.py
+++ mysql-workspace/database.py
@@ -2,18 +2,29 @@ import mysql.connector as mysql
 
 def connect(db_name):
 	try:
-		return mysql.connect(
-			host="localhost",
-			user="root",
-			password="password",
-			database=db_name)
+		return mysql.connect(host="localhost",
+							 database=db_name,
+							 user='root',
+							 password="password")
 	except Error as e:
 		print(e)
 
+def add_project(cursor, project_title, project_description, tasks):
+	pass
+
 if __name__ == '__main__':
 	db = connect("projects")
+
 	cursor = db.cursor()
+
+	tasks = ["Clean bathroom", "Clean kitchen", "Clean living room"]
+	add_project(cursor, "Clean house", "Clean house by room", tasks)
+	db.commit()
+
 	cursor.execute("SELECT * FROM projects")
 	project_records = cursor.fetchall()
 	print(project_records)
-	db.close()
+
+	cursor.execute("SELECT * FROM tasks")
+	tasks_records = cursor.fetchall()
+	print(tasks_records)
