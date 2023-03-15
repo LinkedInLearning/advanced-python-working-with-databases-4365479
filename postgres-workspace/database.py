9b70cb01080cb61e6aeb3d0e6699f932b889ccd7
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-15 13:28:10 -0700

    Update database.py

--- postgres-workspace/database.py
+++ postgres-workspace/database.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
-engine = create_engine('postgresql+psycopg2://postgres:password@localhost/red30')
+engine = create_engine('postgresql://postgres:password@localhost/red30')
 
 # class Sale(Base):
 #       __tablename__='sales',
@@ -19,6 +20,31 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 Sales = Base.classes.sales
+
+with Session(engine) as session:
+
+	# Read
+	smallest_sale = session.execute(select(Sales).order_by(Sales.order_total)).scalar()
+	print(smallest_sale.order_total)
+
+	# Insert
+	recent_sale = Sales(order_num=1105910, cust_name='Syman Mapstone', prod_number='EB521', prod_name='Understanding Artificial Intelligence', quantity=3, price=19.5, discount=0, order_total=58.5)
+	session.add(recent_sale)
+	session.commit()
+
+	# Update
+	recent_sale.quantity = 2
+	recent_sale.order_total = 39
+	updated_sale = session.execute(select(Sales).filter(Sales.order_num == 1105910)).scalar()
+	print(updated_sale.quantity)
+	print(updated_sale.order_total)
+	session.commit()
+
+	# Delete
+	returned_sale = session.execute(select(Sales).filter(Sales.order_num == 1105910)).scalar()
+	session.delete(returned_sale)
+	session.commit()
+
 
 
 
