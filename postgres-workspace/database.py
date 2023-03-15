f53d0ccb0333f0788512d5d06a259c9f896ca985
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-15 13:51:44 -0700

    Update database.py

--- postgres-workspace/database.py
+++ postgres-workspace/database.py
@@ -1,53 +1,88 @@
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Boolean, ForeignKey
+from sqlalchemy.orm import registry, Session, relationship
 
-engine = create_engine('postgresql://postgres:password@localhost/red30')
+engine = create_engine('postgresql://postgres:password@localhost/library', future=True)
 
-# class Sale(Base):
-#       __tablename__='sales',
-#       Column('order_num', Integer, primary_key=true),
-#       Column('cust_name', String),
-#       Column('prod_number', String),
-#       Column('prod_name', String),
-#       Column('quantity', Float),
-#       Column('price', Float),
-#       Column('discount', Float),
-#       Column('order_total', Float))
+mapper_registry = registry()
 
-Base = automap_base()
+Base = mapper_registry.generate_base()
 
-Base.prepare(autoload_with=engine)
+class Author(Base):
+	__tablename__ = 'authors'
+	author_id = Column(Integer, primary_key=True)
+	first_name = Column(String)
+	last_name = Column(String)
+	def __repr__(self):
+		return "<Author(author_id='{0}', first_name='{1}', last_name='{2}')>" \
+			.format(self.author_id, self.first_name, self.last_name)
 
-Sales = Base.classes.sales
+class Book(Base):
+	__tablename__ = 'books'
+       
+	book_id = Column(Integer, primary_key=True)
+	title = Column(String)
+	number_of_pages = Column(Integer)
 
-with Session(engine) as session:
+	def __repr__(self):
+		return "<Book(book_id='{0}', title='{1}', number_of_pages='{2}')>" \
+			.format(self.book_id, self.title, self.number_of_pages)
 
-	# Read
-	smallest_sale = session.execute(select(Sales).order_by(Sales.order_total)).scalar()
-	print(smallest_sale.order_total)
+class BookAuthor(Base):
+	__tablename__ = 'bookauthors'
 
-	# Insert
-	recent_sale = Sales(order_num=1105910, cust_name='Syman Mapstone', prod_number='EB521', prod_name='Understanding Artificial Intelligence', quantity=3, price=19.5, discount=0, order_total=58.5)
-	session.add(recent_sale)
-	session.commit()
+	bookauthor_id = Column(Integer, primary_key=True)
+	author_id = Column(Integer, ForeignKey('authors.author_id'))
+	book_id = Column(Integer, ForeignKey('books.book_id'))
 
-	# Update
-	recent_sale.quantity = 2
-	recent_sale.order_total = 39
-	updated_sale = session.execute(select(Sales).filter(Sales.order_num == 1105910)).scalar()
-	print(updated_sale.quantity)
-	print(updated_sale.order_total)
-	session.commit()
+	author = relationship("Author")
+	book = relationship("Book")
 
-	# Delete
-	returned_sale = session.execute(select(Sales).filter(Sales.order_num == 1105910)).scalar()
-	session.delete(returned_sale)
-	session.commit()
+	def __repr__(self):
+		return "<BookAuthor (bookauthor_id='{0}', author_id='{1}', book_id='{2}', first_name='{3}', last_name='{4}', title='{5}')>" \
+			.format(self.bookauthor_id, self.author_id, self.book_id, self.author.first_name, self.author.last_name, self.book.title)
 
+Base.metadata.create_all(engine)
 
 
+def add_book(title, number_of_pages, first_name, last_name):
+	author = Author(first_name=first_name, last_name=last_name)
+	book = Book(title=title, number_of_pages=number_of_pages)
 
+	with Session(engine, future=True) as session:
+		existing_book = session.execute(select(Book).filter(Book.title==title, Book.number_of_pages==number_of_pages)).scalar()
+		if existing_book is not None:
+			print("Book has already been added. No need to re-add.")
+			return
 
+		session.add(book)
+		existing_author = session.execute(select(Author).filter(Author.first_name==first_name, Author.last_name==last_name)).scalar()
 
+		if existing_author is not None:
+			print("Author exists! Adding book")
+			session.flush()
+			pairing=BookAuthor(author_id=existing_author.author_id, book_id=book.book_id)
+		else:
+			print("Author does not exist. Adding author")
+			session.add(author)
+			session.flush()
+			pairing = BookAuthor(author_id=author.author_id, book_id=book.book_id)
 
+		session.add(pairing)
+		session.commit()
+		print("New book added!")
+		print(author)
+		print(book)
+		print(pairing)
+
+if __name__ == "__main__":
+	print("Input new book:\n")
+	title = input("What is the title of the book?\n")
+	number_of_pages = int(input("How many pages are in the book?\n"))
+	first_name = input("What is the first name of the author?\n")
+	last_name = input("What is the last name of the author?\n")
+	print("Inputting book data:\n")
+
+	#add_book(title, number_of_pages, first_name, last_name)
+
+	print("Done!")
