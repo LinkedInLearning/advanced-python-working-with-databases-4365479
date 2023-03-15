ee9e9bdadca666215da710e77dd2127786a62821
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-15 11:09:32 -0700

    Update database.py

--- postgres-workspace/database.py
+++ postgres-workspace/database.py
@@ -8,14 +8,5 @@ conn = psycopg2.connect(database="red30",
 
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE SALES(ORDER_NUM INT PRIMARY KEY,
-	CUST_NAME TEXT,
-	PROD_NUMBER TEXT,
-	PROD_NAME TEXT,
-	QUANTITY INT,
-	PRICE REAL,
-	DISCOUNT REAL,
-	ORDER_TOTAL REAL);''')
-
 conn.commit()
 conn.close()
